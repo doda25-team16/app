7e97dade796d111a7a5ebe4d6576c672c844022e
Caio Miranda Haschelevici <cmirandahasche@student.tudelft.nl>
2025-12-04 13:30:24 +0100

    Metrics created for monitoring

--- src/main/java/frontend/ctrl/FrontendController.java
+++ src/main/java/frontend/ctrl/FrontendController.java
@@ -2,6 +2,10 @@ package frontend.ctrl;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.env.Environment;
@@ -20,6 +24,17 @@ import jakarta.servlet.http.HttpServletRequest;
 @RequestMapping(path = "/sms")
 public class FrontendController {
 
+    private AtomicInteger totalRequestsSpam = new AtomicInteger(0);
+    private AtomicInteger totalRequestsHam = new AtomicInteger(0);
+    private AtomicInteger totalRequestsFailed = new AtomicInteger(0);
+
+    private AtomicInteger activeRequests = new AtomicInteger(0);
+
+    private AtomicLong requestTimeSum = new AtomicLong(0);
+    private AtomicLong requestTimeCount = new AtomicLong(0);
+    private Map<Double, AtomicInteger> requestTimeBuckets = new TreeMap<>();
+
+
     private String modelHost;
 
     private RestTemplateBuilder rest;
@@ -28,6 +43,12 @@ public class FrontendController {
         this.rest = rest;
         this.modelHost = env.getProperty("MODEL_HOST");
         assertModelHost();
+
+        for (double i = 100; i < 1000; i = i + 100) {
+            requestTimeBuckets.put(i, new AtomicInteger(0));
+        }
+
+        requestTimeBuckets.put(Double.POSITIVE_INFINITY, new AtomicInteger(0));
     }
 
     private void assertModelHost() {
@@ -61,8 +82,34 @@ public class FrontendController {
     @ResponseBody
     public Sms predict(@RequestBody Sms sms) {
         System.out.printf("Requesting prediction for \"%s\" ...\n", sms.sms);
+        activeRequests.incrementAndGet();
+
+        long startTime = System.currentTimeMillis();
         sms.result = getPrediction(sms);
         System.out.printf("Prediction: %s\n", sms.result);
+
+        long timeTaken = System.currentTimeMillis() - startTime;
+        requestTimeSum.addAndGet(timeTaken);
+        requestTimeCount.incrementAndGet();
+
+        for (Map.Entry<Double, AtomicInteger> bucket : requestTimeBuckets.entrySet()) {
+            if (timeTaken <= bucket.getKey()) {
+                bucket.getValue().incrementAndGet();
+                break;
+            }
+        }
+
+        if (sms.result.equals("spam")) {
+            totalRequestsSpam.incrementAndGet();
+        }
+        else if (sms.result.equals("ham")) {
+            totalRequestsHam.incrementAndGet();
+        }
+        else {
+            totalRequestsFailed.incrementAndGet();
+        }
+        activeRequests.decrementAndGet();
+
         return sms;
     }
 
@@ -74,5 +121,34 @@ public class FrontendController {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @GetMapping("/metrics")
+    @ResponseBody
+    private String metrics() {
+        StringBuilder response = new StringBuilder();
+
+        response.append("# TYPE total_requests counter\n");
+        response.append("total_requests{result=\"ham\"} ").append(totalRequestsHam.get()).append("\n");
+        response.append("total_requests{result=\"Failed\"} ").append(totalRequestsFailed.get()).append("\n");
+        response.append("total_requests{result=\"spam\"} ").append(totalRequestsSpam.get()).append("\n\n");
+
+        response.append("# TYPE active_requests gauge\n");
+        response.append("active_requests ").append(activeRequests.get()).append("\n\n");
+
+        // Histogram
+        response.append("# TYPE request_duration_ms histogram\n");
+        for (Map.Entry<Double, AtomicInteger> bucket : requestTimeBuckets.entrySet()) {
+            response.append("request_duration_ms_bucket{le=\"")
+                .append(bucket.getKey().isInfinite() ? "+Inf" : bucket.getKey())
+                .append("\"} ")
+                .append(bucket.getValue().get())
+                .append("\n");
+        }
+
+        response.append("request_duration_sum ").append(requestTimeSum.get()).append("\n");
+        response.append("request_duration_count ").append(requestTimeCount.get()).append("\n");
+
+        return response.toString();
     }
 }
